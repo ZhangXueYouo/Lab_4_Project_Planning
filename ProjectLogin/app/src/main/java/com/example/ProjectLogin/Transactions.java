52f703b5b31cb91f5386c371ef556114b3440c75
Haitao Mao <61724600+ZhangXueYouo@users.noreply.github.com>
2021-02-23 04:51:30 +0800

    Create Transactions.java

---
+++ ProjectLogin/app/src/main/java/com/example/ProjectLogin/Transactions.java
@@ -0,0 +1,13 @@
+package com.example.haitaomao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityTransfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Transactions);
+    }
