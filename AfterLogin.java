922cf67bb51ddf81dc56644c41ff1015148ee4db
Haitao Mao <61724600+ZhangXueYouo@users.noreply.github.com>
2021-03-30 04:13:38 +0800

    Add files via upload

---
+++ AfterLogin.java
@@ -0,0 +1,36 @@
+package com.example.moneytrans;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AfterLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_login);
+    }
+
+
+    public void loginView(View v){ //Intent for going to Login Page
+        Intent login = new Intent(this, MainActivity.class);
+        startActivity(login);
+    }
+
+    public void accountView(View v){ //Intent for going to Account Balance Page
+
+    }
+
+    public void transferView(View v){ //Intent for going to the Transfer Funds Page
+        Intent transfer = new Intent(this, Transfer.class);
+        startActivity(transfer);
+    }
+
+
+
+}
