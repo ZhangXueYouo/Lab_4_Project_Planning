be615ea99a7b019606429bf82a07b9c056535d08
JASLEEN BAINS <67664750+Jasleen-Bains@users.noreply.github.com>
2021-03-29 16:41:40 -0400

    CreateAccount.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,10 +2,7 @@ package com.example.userauthenticationpage;
 //Reference: https://www.youtube.com/watch?v=mPhqDzO7PUU
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
         mPassword = (EditText) findViewById(R.id.password);
         mConfirmPassword = (EditText) findViewById(R.id.confirmpassword);
         mLogin = (TextView) findViewById(R.id.login);
-        mRegister = (TextView) findViewById(R.id.already_register);
-        mLogin.setOnClickListener(new View.OnClickListener()             }
-        });
+        mRegister = (TextView) findViewById(R.id.Registered);
+        //mLogin.setOnClickListener(new View.OnClickListener());
     }
 }
