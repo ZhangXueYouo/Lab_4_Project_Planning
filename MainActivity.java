c835d714bfb513386acc6f471edebf659e0ae85e
JASLEEN BAINS <67664750+Jasleen-Bains@users.noreply.github.com>
2021-03-29 18:14:19 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,16 +2,22 @@ package com.example.userauthenticationpage;
 //Reference: https://www.youtube.com/watch?v=mPhqDzO7PUU
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     EditText mUsername;
     EditText mPassword;
     EditText mConfirmPassword;
-    TextView mLogin;
+    Button mLogin;
     TextView mRegister;
+    TextView mLoginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +27,14 @@ public class MainActivity extends AppCompatActivity {
         mUsername = (EditText) findViewById(R.id.username);
         mPassword = (EditText) findViewById(R.id.password);
         mConfirmPassword = (EditText) findViewById(R.id.confirmpassword);
-        mLogin = (TextView) findViewById(R.id.login);
+        mLogin = (Button)findViewById(R.id.button);
         mRegister = (TextView) findViewById(R.id.Registered);
-        //mLogin.setOnClickListener(new View.OnClickListener());
+        mLoginText = (TextView) findViewById(R.id.login);
+        mLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginIntent = new Intent(MainActivity.this,)
+            }
+        });
     }
 }
