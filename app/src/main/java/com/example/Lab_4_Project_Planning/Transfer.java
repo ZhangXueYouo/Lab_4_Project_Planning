5903743f5b627069e64fe9a0232bd7d7c494554e
Haitao Mao <61724600+ZhangXueYouo@users.noreply.github.com>
2021-03-30 06:16:11 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/Lab_4_Project_Planning/Transfer.java
@@ -0,0 +1,21 @@
+package com.example.moneytrans;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Transfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+    }
+
+    public void Transfer(View view) {
+
+
+    }
+
+}
